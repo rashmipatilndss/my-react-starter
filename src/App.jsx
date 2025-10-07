5b1872031b58ce0fc1cee6dc242f07bdea160947
Rashmi Patil <rashmi.patil1@ndsofttech.com>
2025-10-07 18:01:50 +0530

    cloned and made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import HelloWorld from "./HelloWorld"
+import { useState } from "react";
 
 function App() {
+  const [message,setMessage]=useState("");
+    const handleClick=()=>{
+      setMessage("project running");
+    };
   return (
     <>
     <HelloWorld/>
+    <button onClick={handleClick}>Run Project</button>
+    <p>{message}</p>
     </>
   )
 }
