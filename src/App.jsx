eeab5d1fe3da77fecbd2518d7bdeb87cb436dda9
Rashmi Patil <rashmi.patil1@ndsofttech.com>
2025-10-08 11:34:05 +0530

    counter example added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import HelloWorld from "./HelloWorld"
+import HelloWorld from "./components/Day1/HelloWorld"
+import { useState } from "react";
+import Counter from "./components/Day2/Counter";
 
 function App() {
+  const [message,setMessage]=useState("");
+    const handleClick=()=>{
+      setMessage("project running");
+    };
   return (
     <>
     <HelloWorld/>
+    <Counter intialValue={0}/>
+    <button onClick={handleClick}>Run Project</button>
+    <p>{message}</p>
     </>
   )
 }
